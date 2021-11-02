305eedf3963a6c6f0947392d0bced97fb297dbc1
vemarajumanisindhu <93367854+vemarajumanisindhu@users.noreply.github.com>
2021-11-02 13:53:03 +0530

    Add files via upload

---
+++ raptor code.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main()
+{
+   string raptor_prompt_variable_zzyz;
+   ?? x;
+   ?? y;
+   ?? result;
+
+   raptor_prompt_variable_zzyz ="enter first number";
+   cout << raptor_prompt_variable_zzyz << endl;
+   cin >> x;
+   raptor_prompt_variable_zzyz ="enter the first number ";
+   cout << raptor_prompt_variable_zzyz << endl;
+   cin >> y;
+   result =x+y;
+   cout << "result of the addition operation is "+result << endl;
+   return 0;
+}
