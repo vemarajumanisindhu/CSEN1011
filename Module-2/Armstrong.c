926864db187c58701343537127310fd321ef973e
vemarajumanisindhu <93367854+vemarajumanisindhu@users.noreply.github.com>
2021-12-18 20:48:52 +0530

    Create Armstrong.c

---
+++ Module-2/Armstrong.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main() {
+int n1,remainder,n,result,num;
+printf("enter a three digit number");
+scanf("%d",&n);
+n1=n;
+while(n1!=0) {
+  remainder=n1%10;
+  result+=remainder*remainder*remainder;
+  n1/=10;
+}
+if(result==n)
+printf("%d is am armstrong number",n);
+else 
+printf("%d is not an armstrong number",n);
+return 0;
+}
+
+
+
+
+
